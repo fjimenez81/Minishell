8695aa33a80201f067e1ad60c9415f54251336bd
fernando jimenez cuerva <fernando@fernandos-Mac.local>
2021-01-13 11:46:09 +0100

    VM broken

--- ft_arg_exe.c
+++ ft_arg_exe.c
@@ -6,46 +6,46 @@
 /*   By: fjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 17:23:10 by fjimenez          #+#    #+#             */
-/*   Updated: 2021/01/11 16:46:59 by fjimenez         ###   ########.fr       */
+/*   Updated: 2021/01/13 11:43:46 by fjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	ft_check_path(int *check)
-{
-	int		k;
+// static void	ft_check_path(int *check)
+// {
+// 	int		k;
 
-	k = -1;
-	*check = 0;
-	while (g_envp[++k])
-	{
-		if (ft_strstr(g_envp[k], "/usr/bin:") ||
-			ft_strstr(g_envp[k], "/bin:"))
-			*check = 1;
-	}
-}
+// 	k = -1;
+// 	*check = 0;
+// 	while (g_envp[++k])
+// 	{
+// 		if (ft_strstr(g_envp[k], "/usr/bin:") ||
+// 			ft_strstr(g_envp[k], "/bin:"))
+// 			*check = 1;
+// 	}
+// }
 
-static	int	ft_path(int j)
-{
-	int i;
-	int check;
+// static	int	ft_path(int j)
+// {
+// 	int i;
+// 	int check;
 
-	i = -1;
-	if (j == 3)
-		return (1);
-	while (g_envp[++i])
-	{
-		if (ft_strstr(g_envp[i], "PATH="))
-		{
-			check = 1;
-			if (ft_strrchr(g_envp[i], '=') + 1)
-				ft_check_path(&check);
-			return (check);
-		}
-	}
-	return (0);
-}
+// 	i = -1;
+// 	if (j == 3)
+// 		return (1);
+// 	while (g_envp[++i])
+// 	{
+// 		if (ft_strstr(g_envp[i], "PATH="))
+// 		{
+// 			check = 1;
+// 			if (ft_strrchr(g_envp[i], '=') + 1)
+// 				ft_check_path(&check);
+// 			return (check);
+// 		}
+// 	}
+// 	return (0);
+// }
 
 void		ft_print_error(char *error)
 {
@@ -54,27 +54,85 @@ void		ft_print_error(char *error)
 	ft_putendl_fd(": command not found", 1);
 }
 
-static void	ft_arg_exe_aux(t_shell *pcs, t_test *tst, int i)
+char	**ft_path_split(void)
+{
+	int i;
+
+	i = -1;
+	while (g_envp[++i])
+	{
+		if (ft_strstr(g_envp[i], "PATH="))
+			return (ft_split(ft_strrchr(g_envp[i], '=') + 1, ':'));
+	}
+	return (NULL);
+}
+
+char **ft_add_str(char **s)
+{
+	int i;
+	char **aux;
+
+	if (!s || (!(aux = (char**)malloc(sizeof(char*) * (ft_len_tab(s) + 2)))))
+		return (NULL);
+	i = -1;
+	while(s[++i])
+		aux[i] = ft_join_char(s[i], '/');
+	aux[i] = ft_strdup("");
+	i++;
+	aux[i] = NULL;
+	ft_free_tab(s);
+	return (aux);
+}
+
+void ft_exe_path(t_shell *pcs, t_test *tst, int i, char **aux)
+{
+	int		exe;
+
+	exe = execve(pcs->cmp[0], pcs->cmp, g_envp);
+	if (exe == -1)
+	{
+		dup2(pcs->std_out, 1);
+		ft_err_exit(pcs, tst, i);
+		ft_free_tab(aux);
+		ft_free_tab(pcs->cmp);
+		exit(127);
+	}
+}
+
+void ft_exe_cmd(t_shell *pcs, t_test *tst, int i, char **aux)
 {
 	char	*join;
+	char	**tmp;
 	int		exe;
 	int		j;
 
 	exe = -1;
-	j = 0;
-	while (exe == -1 && j < 3)
+	j = -1;
+	tmp = ft_add_str(aux);
+	while (exe == -1 && ++j < ft_len_tab(tmp))
 	{
-		join = ft_strjoin(tst->paths[j], pcs->cmp[0]);
-		j++;
-		if (ft_path(j))
-			exe = execve(join, pcs->cmp, g_envp);
+		join = ft_strjoin(tmp[j], pcs->cmp[0]);
 		free(join);
-		if (j == 3 && exe == -1)
+		exe = execve(join, pcs->cmp, g_envp);
+		if (j == ft_len_tab(tmp) - 1 && exe == -1)
 		{
 			dup2(pcs->std_out, 1);
 			ft_err_exit(pcs, tst, i);
 		}
 	}
+	ft_free_tab(tmp);
+}
+
+static void	ft_arg_exe_aux(t_shell *pcs, t_test *tst, int i)
+{
+	
+	char	**aux;
+	
+	aux = ft_path_split();
+	if (aux == NULL)
+		ft_exe_path(pcs, tst, i, aux);
+	else
+		ft_exe_cmd(pcs, tst, i, aux);
 }
 
 int			ft_arg_exe(t_shell *pcs, t_test *tst, int i)
--- ft_split_cmd.c
+++ ft_split_cmd.c
@@ -6,11 +6,141 @@
 /*   By: fjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/16 16:48:04 by fernando          #+#    #+#             */
-/*   Updated: 2021/01/11 20:03:08 by fjimenez         ###   ########.fr       */
+/*   Updated: 2021/01/13 11:00:40 by fjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// static	int	getcount(t_test *t, char *s, int count, int c)
+// {
+// 	t->k = 0;
+// 	while (s[t->k] != '\0')
+// 	{
+// 		if (s[t->k] == '\"')
+// 		{
+// 			t->k++;
+// 			while (s[t->k] && s[t->k] != '\"')
+// 				t->k++;
+// 		}
+// 		if (s[t->k] == '\'')
+// 		{
+// 			t->k++;
+// 			while (s[t->k] && s[t->k] != '\'')
+// 				t->k++;
+// 		}
+// 		if (s[t->k] == '\0')
+// 			return (count);
+// 		if (s[t->k] && (s[t->k - 1] != c && s[t->k] == c))
+// 			count++;
+// 		t->k++;
+// 	}
+// 	if (s[t->k - 1] != c)
+// 		count++;
+// 	return (count);
+// }
+
+// static int	ft_wordcount(t_test *t, char *s, char c)
+// {
+// 	int count;
+
+// 	count = 0;
+// 	t->k = 0;
+// 	if (s[t->k] == '\0')
+// 		return (0);
+// 	t->k++;
+// 	if (s[t->k] == '\0' && s[t->k - 1] != c)
+// 		return (1);
+// 	count = getcount(t, s, count, c);
+// 	return (count);
+// }
+
+// static int	ft_wordlen(t_test *t, char *s, char c)
+// {
+// 	int count;
+
+// 	count = 0;
+// 	t->k = 0;
+// 	while (s[t->k] != '\0' && s[t->k] != c)
+// 	{
+// 		if (*s == '\"')
+// 		{
+// 			count++;
+// 			t->k++;
+// 			while (s[t->k] && s[t->k] != '\"' && (count = count + 1))
+// 				t->k++;
+// 		}
+// 		if (s[t->k] == '\'')
+// 		{
+// 			t->k++;
+// 			count++;
+// 			while (s[t->k] && s[t->k] != '\'' && (count = count + 1))
+// 				t->k++;
+// 		}
+// 		// if (s[t->k] == 92 && s[t->k + 1] == 92)
+// 		// {
+// 		// 	t->k += 2;
+// 		// 	count += 2;
+// 		// }
+// 		// if (s[t->k] == 92 && s[t->k + 1] == c)
+// 		// {
+// 		// 	t->k += 2;
+// 		// 	count += 2;
+// 		// }
+// 		count++;
+// 		if (s[t->k] == 0)
+// 			return (count);
+// 		t->k++;
+// 	}
+// 	return (count);
+// }
+
+// static char	**splitter(t_test *t, char const *s, char c)
+// {
+// 	char	**tmp;
+// 	int		len;
+
+// 	t->i = 0;
+// 	//t->k = -1;
+// 	tmp = ft_calloc((ft_wordcount(t, (char*)s, c) + 1), sizeof(char*));
+// 	while (*s != '\0')
+// 	{
+// 		while (*s == c)
+// 			s++;
+// 		t->j = 0;
+// 		len = (ft_wordlen(t, (char*)s, c) + 1);
+// 		tmp[t->i] = ft_calloc(len, sizeof(char));
+// 		while (--len && *s != '\0')
+// 		{
+// 			tmp[t->i][t->j] = *s;
+// 			t->j++;
+// 			s++;
+// 		}
+// 		tmp[t->i][t->j] = '\0';
+// 		t->i++;
+// 	}
+// 	tmp[t->i] = NULL;
+// 	return (tmp);
+// }
+
+// char		**ft_split_cmd(char const *s, char c)
+// {
+// 	char	**tmp;
+// 	t_test	t;
+
+// 	if (!s)
+// 		return (NULL);
+// 	if (ft_strcmp(s, "") == 0)
+// 	{
+// 		if (!(tmp = malloc(sizeof(char*))))
+// 			return (NULL);
+// 		*tmp = NULL;
+// 		return (tmp);
+// 	}
+// 	if (!(tmp = splitter(&t, s, c)))
+// 		return (NULL);
+// 	return (tmp);
+// }
 
 static	int	getcount(char *s, int count, int c)
 {
@@ -30,8 +160,6 @@ static	int	getcount(char *s, int count, int c)
 		}
 		if (*s == '\0')
 			return (count);
-		if (*s && (*(s - 1) == c && *s == c))
-			s++;
 		if (*s && (*(s - 1) != c && *s == c))
 			count++;
 		s++;
@@ -60,17 +188,25 @@ static int	ft_wordlen(char *s, char c)
 	int count;
 
 	count = 0;
-	while (*s != '\0')
+	while (*s != '\0' && *s != c)
 	{
-		ft_wordlen_aux((char*)s, &count);
-		if (*s == 92 && *(s + 1) == c)
+		if (*s == '\"')
 		{
 			count++;
 			s++;
+			while (*s && *s != '\"' && (count = count + 1))
+				s++;
 		}
-		else if (*s == c && *(s - 1) != 92)
-			break ;
+		if (*s == '\'')
+		{
+			s++;
+			count++;
+			while (*s && *s != '\'' && (count = count + 1))
+				s++;
+		}
 		count++;
+		if (*s == 0)
+			return (count);
 		s++;
 	}
 	return (count);
@@ -80,45 +216,52 @@ static char	**splitter(char const *s, char c)
 {
 	int		i;
 	int		j;
-	char	**tmp;
+	char	**t;
 	int		len;
 
 	i = 0;
-	tmp = ft_calloc((ft_wordcount((char*)s, c) + 1), sizeof(char*));
+	t = ft_calloc((ft_wordcount((char*)s, c) + 1), sizeof(char*));
 	while (*s != '\0')
 	{
 		while (*s == c)
 			s++;
 		j = 0;
 		len = (ft_wordlen((char*)s, c) + 1);
-		tmp[i] = ft_calloc(len, sizeof(char));
+		t[i] = ft_calloc(len, sizeof(char));
 		while (--len && *s != '\0')
 		{
-			tmp[i][j] = *s;
+			t[i][j] = *s;
 			j++;
 			s++;
 		}
-		tmp[i][j] = '\0';
+		t[i][j] = '\0';
 		i++;
 	}
-	tmp[i] = NULL;
-	return (tmp);
+	t[i] = NULL;
+	return (t);
 }
 
 char		**ft_split_cmd(char const *s, char c)
 {
-	char	**tmp;
+	char	*trimmed;
+	char	totrim[2];
+	char	**t;
 
 	if (!s)
 		return (NULL);
 	if (ft_strcmp(s, "") == 0)
 	{
-		if (!(tmp = malloc(sizeof(char*))))
+		if (!(t = malloc(sizeof(char*))))
 			return (NULL);
-		*tmp = NULL;
-		return (tmp);
+		*t = NULL;
+		return (t);
 	}
-	if (!(tmp = splitter(s, c)))
+	totrim[0] = c;
+	totrim[1] = '\0';
+	if (!(trimmed = ft_strtrim(s, totrim)))
 		return (NULL);
-	return (tmp);
+	if (!(t = splitter(trimmed, c)))
+		return (NULL);
+	free(trimmed);
+	return (t);
 }
